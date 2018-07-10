bf4e7f7b387f3bad1a0ab546a1a8f7f17bade04e
= <s3642807@student.rmit.edu.au>
2018-07-10 19:56:17 +1000

    init

---
+++ Role.go
@@ -0,0 +1,27 @@
+package rolextract
+
+//Role holds both allow and deny policies
+type Role struct {
+	AllowPolicy Policy
+	DenyPolicy  Policy
+}
+
+//Policy holds permissions grouped by resource group
+type Policy struct {
+	Groups map[string]map[string]map[string]map[string][]*Permission //client-id -> platform -> resource group -> resource name -> Resource
+}
+
+//Permission holds the permissions of a certain resources
+type Permission struct {
+	Actions    []string
+	Resources  []*Resource
+	AllActions bool `json:"-"`
+}
+
+//Resource holds the detail of the permission
+type Resource struct {
+	Target     string                 // id / *
+	Conditions map[string]string      // for groupings
+	Children   map[string]*Permission //children of the resource
+	All        bool                   `json:"-"`
+}
