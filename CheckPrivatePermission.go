a05f6417fbbc4d960c7c87f58b2b150f1d59739f
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-10-18 13:55:28 +1100

    add checkprivatepermission

---
+++ CheckPrivatePermission.go
@@ -0,0 +1,23 @@
+package perm
+
+import (
+	"strconv"
+
+	"github.com/kkesley/arn"
+	"github.com/kkesley/iteacloud-jwt/jwtidentity"
+)
+
+//CheckPrivatePermission check if a user can access a private resource.
+//They can access the resource if the resource is themselves
+//They can access the resource if the resource is their client only if they are the root
+//They can access the resource if the resource is their peers only if they are the root
+func CheckPrivatePermission(ARN string, token jwtidentity.Token) bool {
+	if token.ClientARN == ARN && !token.IsRoot {
+		return false
+	} else if token.UserARN != ARN && !token.IsRoot {
+		return false
+	} else if arn.GetPartResourceID(ARN, "itea") != strconv.FormatUint(token.ClientID, 10) {
+		return false
+	}
+	return true
+}
