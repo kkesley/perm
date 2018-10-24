1d599e48717ecff61260bc4d16c7cb333736a996
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-10-24 16:21:36 +1100

    add filter id

---
+++ PermissionFilterIDs.go
@@ -0,0 +1,57 @@
+package perm
+
+import (
+	"strings"
+
+	"github.com/jinzhu/gorm"
+)
+
+//PermissionFilterIDs return query for filtering group. Note that the table field must be the same
+//field must have
+//arn (primary key)
+//resource_group (index)
+//group_key (primary key)
+//group_value
+//created_at
+func (p CheckResponse) PermissionFilterIDs(db *gorm.DB, arnGroup string) *gorm.DB {
+	conditions := make([]map[string]string, 0)
+	if p.Allow.All {
+		conditions = p.Allow.Conditions
+	} else if p.Deny.All {
+		conditions = p.Deny.Conditions
+	}
+
+	if len(conditions) <= 0 {
+		return nil
+	}
+
+	groupQuery := db.Select([]string{"arn"}).Where("active = ?", true).Where("resource_group = ?", arnGroup)
+	conditionQuery := make([]string, 0)
+	conditionObj := make([]interface{}, 0)
+
+	havingQueryExt := make([]string, 0)
+	isCondition := false
+	for _, condition := range conditions {
+		havingQuery := make([]string, 0)
+		for key, val := range condition {
+			if len(key) <= 0 || len(val) <= 0 {
+				continue
+			}
+			conditionQuery = append(conditionQuery, "(group_key = ? AND group_value = ?)")
+			conditionObj = append(conditionObj, []interface{}{key, val}...)
+
+			havingQuery = append(havingQuery, "SUM(group_key = ? AND group_value = ?) > 0")
+			isCondition = true
+		}
+		havingQueryExt = append(havingQueryExt, "("+strings.Join(havingQuery, " AND ")+")")
+	}
+
+	if !isCondition {
+		return nil
+	}
+
+	groupQuery = groupQuery.Having(strings.Join(havingQueryExt, " OR "), conditionObj...)
+	groupQuery = groupQuery.Where(strings.Join(conditionQuery, " OR "), conditionObj...)
+	groupQuery = groupQuery.Group("arn")
+	return groupQuery
+}
